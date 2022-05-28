928f22b53fd8f17292111a1b67cde0e1b219c4f7
scm-aunglinnaing <scmaunglinnaing@gmail.com>
2022-05-28 10:53:36 +0630

    test: add plain-modal

---
+++ src/app/components/plain-modal/plain-modal.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-plain-modal',
+  templateUrl: './plain-modal.component.html',
+  styleUrls: ['./plain-modal.component.scss']
+})
+export class PlainModalComponent implements OnInit {
+  content?: string;
+  note?: string;
+  applyText?: string;
+  constructor(
+    private dialogRef: MatDialogRef<PlainModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: PlainModalComponent
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+
+}
