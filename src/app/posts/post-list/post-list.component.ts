f43162a31bf88b08efd804a05f00cc9eac539b07
Saulid <lwinh80@gmail.com>
2022-06-07 11:15:23 +0630

    Squashed commit of the following:
    commit ff215ad6a319b0b9e8dabc94c865960160bdf488
    Author: Saulid <lwinh80@gmail.com>
    Date:   Tue Jun 7 11:00:36 2022 +0630
    
        #hol_post-list
    
    commit 0d2c0ec1c7d80136c6db67ed6a43f94ebdbc41fb
    Author: Saulid <lwinh80@gmail.com>
    Date:   Mon Jun 6 13:38:36 2022 +0630
    
        #hol_post-list
    
    commit 328edf5d7e9e82219c8543b58263bc1f4ffaff24
    Author: Saulid <lwinh80@gmail.com>
    Date:   Mon Jun 6 13:24:58 2022 +0630
    
        #hol_post-list
    
    commit a818d5d14a07ccc682eac62f7072832ddca2a1f1
    Author: Saulid <lwinh80@gmail.com>
    Date:   Sun Jun 5 23:10:28 2022 +0630
    
        #hol_post_list
    
    commit 7c3958f7de0e001d2cb56de8227d054ded8224cf
    Author: Saulid <lwinh80@gmail.com>
    Date:   Sat Jun 4 23:20:37 2022 +0630
    
        Update post-modal.component.html
    
    commit 5955e5cafd099a1725b66bf21612adcccb5313f9
    Author: Saulid <lwinh80@gmail.com>
    Date:   Fri Jun 3 16:04:43 2022 +0630
    
        #hol_post-list
    
    commit b09228245f4e550c9739266a180798ff3e2b229d
    Author: Saulid <lwinh80@gmail.com>
    Date:   Fri Jun 3 10:21:34 2022 +0630
    
        post-data-delete
    
    commit df47f2949f1e526d6b03a6a8bebdb7a28d0fec8c
    Author: Saulid <lwinh80@gmail.com>
    Date:   Thu Jun 2 23:44:15 2022 +0630
    
        post-list
    
    commit a1b936f8544b306c87417e2763cf81932b290c10
    Author: Saulid <lwinh80@gmail.com>
    Date:   Thu Jun 2 18:30:10 2022 +0630
    
        #hol_post-list
    
    commit 25271e388a5bda3f675e717e0c0376ff465b8050
    Author: Saulid <lwinh80@gmail.com>
    Date:   Thu Jun 2 10:32:30 2022 +0630
    
        Update db.json
    
    commit 810a7cf2db68416ec805f8bf633ce490b1a9e085
    Author: Saulid <lwinh80@gmail.com>
    Date:   Wed Jun 1 23:30:35 2022 +0630
    
        #hol_post-list
    
    commit 59f39c5aeafcfdc8d288e8d8c9b445ea83dcf8eb
    Author: Saulid <lwinh80@gmail.com>
    Date:   Wed Jun 1 16:37:35 2022 +0630
    
        #hol_post-list
    
    commit ab774934c38a0e4337a5b6d1caa159af89145a23
    Author: Saulid <lwinh80@gmail.com>
    Date:   Tue May 31 20:37:54 2022 +0630
    
        #hol_post-list
    
    commit 37b3cbba2d74974933b6e05527a68450b999ba10
    Author: Saulid <lwinh80@gmail.com>
    Date:   Tue May 31 11:29:58 2022 +0630
    
        update
    
    commit 76ebe3232f701967c93109fd5628a7c1eb4d2c4a
    Author: Saulid <lwinh80@gmail.com>
    Date:   Mon May 30 22:31:56 2022 +0630
    
        #hol_post_list_update
    
    commit 0e6f60603eeed1a2e7d0b298d373d3dff0a20a69
    Author: Saulid <lwinh80@gmail.com>
    Date:   Mon May 30 22:24:08 2022 +0630
    
        Update post-list.component.html
    
    commit eeaebf4060341831fe144c2a7096e8763aa9e43d
    Merge: fc2938c c8365e7
    Author: Saulid <lwinh80@gmail.com>
    Date:   Mon May 30 18:16:24 2022 +0630
    
        Merge branch 'feature/#hol_post_list' of https://github.com/scm-aunglinnaing/Angular_BulletinBoard into feature/#hol_post_list
    
    commit fc2938c794b6ae4434f921c909f3214afe24ee3b
    Author: Saulid <lwinh80@gmail.com>
    Date:   Mon May 30 18:14:42 2022 +0630
    
        #hol_post_update

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,22 +1,12 @@
 import { ViewChild, Component, OnInit, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { CdkTableExporterModule } from 'cdk-table-exporter';
-import { MatTableExporterModule, MatTableExporterDirective } from 'mat-table-exporter';
 import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostModalComponent } from 'src/app/components/post-modal/post-modal.component';
 import { MatDialog } from '@angular/material/dialog';
-
-//pages
-import { UploadCsvComponent } from '../upload-csv/upload-csv.component';
 
 //services
 import { PostService } from 'src/app/services/post.service';
-
-export interface PeriodicElement {
-  name: string;
-  description: string;
-  created_user_id: string;
-  created_date: number;
-}
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -25,42 +15,124 @@ export interface PeriodicElement {
 
 export class PostListComponent implements OnInit {
 
-  dataSource!: MatTableDataSource<PeriodicElement>;
-  posts: any;
-  displayedColumns: string[] = ['title', 'description', 'created_user_id', 'created_date'];
+  public postDetail: any = [];
+  public allPost: any = [];
+  public eachPost: any = [];
+  public eachData: any = [];
+  public userInfo: any = [];
+  public postListDetail: any = [];
+  public postId: any;
+
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['title', 'description', 'created_user_id', 'created_at', 'action'];
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private postSvc: PostService,
-    private dialog: MatDialog) { }
+  constructor(private postSvc: PostService, private router: Router, public dialog: MatDialog, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.login();
+    
+  }
 
-    this.postSvc.geAllPost().subscribe((data) => {
-      console.log(data);
-      this.posts = data;
-      this.dataSource = new MatTableDataSource(this.posts);
-      this.dataSource.paginator = this.paginator;
+  login() {
+    this.userInfo = JSON.parse(localStorage.getItem('userInfo') || '[]');
+    if (this.userInfo.type === 0) {
+      this.getPostData();
+    } else {
+      this.getEachPost();
+      }
+  }
+
+  getPostData() {
+    this.postSvc.getPost().subscribe({
+      next: posts => {
+        this.allPost = posts.filter((data: any) => {
+          return data.is_removed == false;
+        });
+        this.dataSource = new MatTableDataSource(this.allPost);
+        this.dataSource.paginator = this.paginator;
+      }
     });
   }
+  getEachPost() {
+    this.postSvc.getPost().subscribe({
+      next: posts => {
+        this.postListDetail = posts.filter((data: any) => {
+          return data.created_user_id === this.userInfo.id && data.is_removed == false;
+        });
+        console.log(this.postListDetail);
+        this.dataSource = new MatTableDataSource(this.postListDetail);
+        this.dataSource.paginator = this.paginator;
+      }
+    });
+  }
+  
+
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim();
     filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
   }
+  createPost() {
+    this.router.navigate(['/post']);
+  }
+  editPost(postId: number) {
+    this.router.navigate(['/post/'+ postId])
+  }
 
-  uploadCSV() {
-    let dialogRef = this.dialog.open(UploadCsvComponent, {
-      width: '500px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      this.postSvc.geAllPost().subscribe((data) => {
-        this.posts = data;
-        this.dataSource = new MatTableDataSource(this.posts);
-        this.dataSource.paginator = this.paginator;
-      })
+  deletePost(postId: any) {
+    this.postSvc.getPostDetail(postId).subscribe({
+      next: data => {
+        this.eachData = data;
+        const param = {
+          "title": this.eachData.title,
+          "description": this.eachData.description,
+          "status": this.eachData.status,
+          "created_user_id": this.eachData.created_user_id,
+          "updated_user_id": this.eachData.updated_user_id,
+          "created_at": this.eachData.created_at,
+          "is_removed": true,
+          "deleted_at": new Date()
+        }
+        this.postSvc.deletePost(postId, param).subscribe({
+          next: data => { 
+            if (this.userInfo.type === 0) {
+              this.getPostData();
+            }
+            else {
+              this.getEachPost();
+            }
+          }
+        })
+      }
     })
   }
+
+  titleDetail(postId: any) {
+
+      this.postSvc.getPostDetail(postId).subscribe({
+        next: res => {
+          this.eachData = res;
+          this.dialog.open(PostModalComponent, {
+            width: '100%',
+            data: {
+              title: res.title,
+              description: res.description,
+              status: res.status,
+              created_user_id: res.created_user_id,
+              updated_user_id: res.updated_user_id,
+              created_at: res.created_at,
+            }
+          });
+        },
+        error: err => {
+          console.log('=== handle error ===');
+          console.log(err);
+        }
+      });
+    }
+
 }
 
 
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import * as FileSaver from 'file-saver';
-import { RouterLinkWithHref } from '@angular/router';
-
-const CSV_EXTENSION = '.csv';
-const CSV_TYPE = 'text/plain;charset=utf-8';
 
 @Injectable({
   providedIn: 'root'
@@ -15,14 +10,33 @@ export class PostService {
   apiUrl = 'http://localhost:3000';
   constructor(private http: HttpClient) { }
 
+
+
+
+
+  getPost(): Observable<any> {
+    const url = this.apiUrl + '/posts';
+    return this.http.get(url);
+  }
+  deletePost(postId: any, data: any): Observable<any> {
+    const url = this.apiUrl + '/posts/' + postId;
+    return this.http.put(url, data);
+  }
+  getPostDetail(postId: any): Observable<any> {
+    const url = this.apiUrl + '/posts/' + postId;
+    return this.http.get(url);
+  }
   geAllPost(): Observable<any> {
     const url = this.apiUrl + '/posts';
     return this.http.get(url);
   }
+  
+  
 
-  getPostDetail(postId: number): Observable<any> {
+  async getListDetail(postId: number): Promise<object> {
     const url = this.apiUrl + '/posts/' + postId;
-    return this.http.get(url);
+    console.log(postId);
+    return await this.http.get(url);
   }
 
   createPost(data: any): Observable<any> {
@@ -35,54 +49,4 @@ export class PostService {
     return this.http.put(url, data);
   }
 
-//  downloadFile(data:any, filename = 'data') {
-//    let csvData = this.ConvertToCSV(data, [
-//        'Post Title', 'Post description', 'Posted User', 'Posted Date'
-//    ]);
-//    console.log(typeof(csvData))
-//    let blob = new Blob(['\ufeff' + csvData], {
-//        type: 'text/csv;charset=utf-8;'
-//    });
-//    let dwldLink = document.createElement("a");
-//    let url = URL.createObjectURL(blob);
-//    //let isSafariBrowser = navigator.userAgent.indexOf(
-//    //    'Safari') != -1 & amp; & amp;
-//    //navigator.userAgent.indexOf('Chrome') == -1;
-//    //if Safari open in new window to save file with random filename.
-//    //if (isSafariBrowser) {
-//    //
-//    //    dwldLink.setAttribute("target", "_blank");
-//    //}
-//    dwldLink.setAttribute("href", url);
-//    dwldLink.setAttribute("download", filename + ".csv");
-//    dwldLink.style.visibility = "hidden";
-//    document.body.appendChild(dwldLink);
-//    dwldLink.click();
-//    document.body.removeChild(dwldLink);
-//}
-//ConvertToCSV(objArray:any, headerList:any) {
-//  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-//  var str = '';
-//  var row = '';
-//
-//  for (var index in headerList) { //objArray[0]
-//      //Now convert each value to string and comma-separated
-//      row += headerList[index] + ',';
-//  }
-//  row = row.slice(0, -1);
-//  //append Label row with line break
-//  str += row + '\r\n';
-//  for (var i = 0; i < array.length; i++) {
-//      var line = (i+1)+'';
-//      for (var index in headerList) {//array[i]
-//         let head = headerList[index];
-//
-//          //if (line != '') line += ','
-//          line += ',' + array[i][head];
-//      }
-//      str += line + '\r\n';
-//  }
-//  console.log(str)
-//  return str;
-//}
 }
